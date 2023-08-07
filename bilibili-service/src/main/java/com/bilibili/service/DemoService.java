78f95fb4271823af41925765aeae8809cf6a01bb
1127599549 <1127599549.com>
2023-08-08 00:22:25 +0800

    实现热部署

--- bilibili-service/src/main/java/com/bilibili/service/DemoService.java
+++ bilibili-service/src/main/java/com/bilibili/service/DemoService.java
@@ -4,6 +4,8 @@ import com.bilibili.dao.DemoDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class DemoService {
     @Autowired
