3822682b4ca9dd0ece34514d1045f74620a59bbb
Anoceans <1127599549@qq.com>
2023-08-18 17:11:45 +0800

    实现用户注册接口

--- bilibili-api/src/main/java/com/bilibili/api/UserApi.java
+++ bilibili-api/src/main/java/com/bilibili/api/UserApi.java
@@ -1,10 +1,13 @@
 package com.bilibili.api;
 
 import com.bilibili.domain.JsonResponse;
+import com.bilibili.domain.User;
 import com.bilibili.service.UserService;
 import com.bilibili.service.util.RSAUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +21,9 @@ public class UserApi {
         return new JsonResponse<>(pk);
     }
 
-
+    @PostMapping("/users")
+    public JsonResponse<String> addUser(@RequestBody User user) {
+        userService.addUser(user);
+        return JsonResponse.success();
+    }
 }
---
+++ bilibili-dao/src/main/java/com/bilibili/domain/constant/UserConstant.java
@@ -0,0 +1,9 @@
+package com.bilibili.domain.constant;
+
+public class UserConstant {
+    public static String GENDER_MALE = "0";
+    public static String GENDER_FEMALE = "1";
+    public static String GENDER_UNKNOW = "0";
+    public static String DEFAULT_BIRTH = "1998-03-31";
+    public static String DEFAULT_NICK = "苏苏";
+}
