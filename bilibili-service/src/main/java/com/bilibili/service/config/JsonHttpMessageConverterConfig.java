a54cb488b092541970b73b9d9e1e3746944fc291
Anoceans <1127599549@qq.com>
2023-08-18 15:01:00 +0800

    添加Json信息转换配置，全局异常处理配置

---
+++ bilibili-service/src/main/java/com/bilibili/service/config/JsonHttpMessageConverterConfig.java
@@ -0,0 +1,44 @@
+package com.bilibili.service.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonContainer;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class JsonHttpMessageConverterConfig {
+
+//    public static void main(String[] args) {
+//        List<Object> list = new ArrayList<>();
+//    }
+
+    @Bean
+    @Primary
+    // 对Http方法/结果请求和数据做转换的工具类
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        fastJsonConfig.setSerializerFeatures(
+                // Json 格式化输出设置
+                SerializerFeature.PrettyFormat,
+                // 把 NUll 的值显式显示
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteMapNullValue,
+                // 默认升序排序
+                SerializerFeature.MapSortField,
+                // 禁用循环引用
+                SerializerFeature.DisableCircularReferenceDetect
+        );
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        return new HttpMessageConverters(fastConverter);
+    }
+}
