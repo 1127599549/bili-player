92c21f9c5ae06f5323c4d3c15e29c53902d2b59a
Anoceans <1127599549@qq.com>
2023-08-18 15:45:42 +0800

    定义用户和用户信息表结构以及获取RSA接口开发

---
+++ bilibili-api/src/main/java/com/bilibili/api/UserApi.java
@@ -0,0 +1,22 @@
+package com.bilibili.api;
+
+import com.bilibili.domain.JsonResponse;
+import com.bilibili.service.UserService;
+import com.bilibili.service.util.RSAUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserApi {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/rsa-pks")
+    public JsonResponse<String> getRsaPublicKey() {
+        String pk = RSAUtil.getPublicKeyStr();
+        return new JsonResponse<>(pk);
+    }
+
+
+}
---
+++ bilibili-service/src/main/java/com/bilibili/service/UserService.java
@@ -0,0 +1,13 @@
+package com.bilibili.service;
+
+import com.bilibili.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+}
